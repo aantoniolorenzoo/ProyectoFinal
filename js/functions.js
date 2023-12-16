fc5fb11242d4c8eba3f6131153533b1e536286ca
aantoniolorenzoo <acanjim2704@g.educaand.es>
2023-12-16 23:16:09 +0100

    entrega final

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,24 @@
+// Obtén referencias a elementos del DOM
+var modal = document.getElementById('myModal');
+var openModalBtn = document.getElementById('openModal');
+var closeModalBtn = document.getElementsByClassName('close')[0];
+
+// Abre la modal cuando se hace clic en el enlace "Contacts"
+openModalBtn.onclick = function() {
+  modal.style.display = 'block';
+}
+
+// Cierra la modal cuando se hace clic en la "x"
+closeModalBtn.onclick = function() {
+  modal.style.display = 'none';
+}
+
+// Cierra la modal si se hace clic fuera de ella
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+}
 
 
 
